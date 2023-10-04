96731605f1e52ffa3d8f80a79a82227324b05b63
xzaborskyb <xzaborskyb@stuba.sk>
2023-10-04 13:21:47 +0200

    added set of visited urls

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,16 @@
 from . import *
 
 def main(curr_url):
+    visited = set()
     # queue for BFS
     queue = [curr_url]
     while queue:
+        # TODO check for already visited websites
         target = queue.pop(0)
         # contents of target page
         page = requests.get(target)
         soup = bs(page.text, 'html.parser')
+        visited.add(target)
         # find all hrefs and put them into a list
         links = [a.get('href') for a in soup.find_all('a', href=True)]
 
@@ -22,9 +25,11 @@ def main(curr_url):
 
 
 if __name__ == '__main__':
-    curr_url ="https://pitwall.app/" 
+    curr_url ="https://pitwall.app/"
     main(curr_url)
 
 # TODO plan
 #   Each website will be used to list all links which will be added to a queue.
 #   We will iterate through the queue to visit all websites
+#   make a table with words their frequency and code of ducment in which the words is located
+#   implement potrer - reduce all words to their stems == easier look up
