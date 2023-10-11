84550de579a463cf142c01b601a9dd08767264e6
xzaborskyb <xzaborskyb@stuba.sk>
2023-10-11 15:09:20 +0200

    added new regex in comments

--- src/url_handler_scripts.py
+++ src/url_handler_scripts.py
@@ -3,7 +3,7 @@ from . import *
 import re
 
 PAGE_BLACK_LIST = r'(twitter|facebook|instagram|paypal|linkedin)\.com|/.*\.php.*|.+#.+'
-
+# (?<=\")(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)|(/.+)(?=\")
 def get_absolute(curr_url, url):
     parsed_url = urlparse(url)
     if not bool(parsed_url.netloc):
