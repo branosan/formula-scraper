3bfaf0b1e8cc4dd36b61e260ce162c49b546b5df
xzaborskyb <xzaborskyb@stuba.sk>
2023-10-11 10:29:24 +0200

    added new todo

--- src/__main__.py
+++ src/__main__.py
@@ -11,16 +11,12 @@ if __name__ == '__main__':
     crawler.crawl()
     # construct blacklist for this crawler
 
-
-    # main(curr_url, max_depth)
-
 # TODO plan
 #   Each website will be used to list all links which will be added to a queue.
 #   We will iterate through the queue to visit all websites
 #   make a table with words their frequency and code of ducment in which the words is located
 #   implement potrer - reduce all words to their stems == easier look up
 
-# contents of target page
-# check if page is for a f1 circuit (contains "-grand-prix")
-# if it does contain grand prix find wiki page for this grand-prix by modifing current xxx-grand-prix to
-# Xxx_Grand_Prix and concat with wikipedia page
+# TODO 
+# natahat html zo vsetkych stranok nemenit ich
+# vytvorit index z html
